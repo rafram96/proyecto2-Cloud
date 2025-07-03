57476aa25b86900ce41362ac8d54e20a4f9f338c
tamitooo <tamy.flores@utec.edu.pe>
2025-07-03 18:30:00 -0500

    falta cart y orders

--- frontend-test/src/pages/Myorders.tsx
+++ frontend-test/src/pages/Myorders.tsx
@@ -4,11 +4,6 @@ const Myorders: React.FC = () => {
   return (
     <div className="flex flex-col  justify-items-center h-screen ">
 
-        <section className="text-center">
-          <h1 className="font-koulen font-bold text-[128px] text-[#FCF7D5] ">Up to 30% off laptops
-          </h1>
- 
-        </section>
               
     </div>
   );
